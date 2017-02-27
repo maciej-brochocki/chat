bd935c019a3d3993b4c834f0cccb66d496b9c358
Maciej Brochocki <maciej.brochocki@bestideas.pl>
2017-02-27 16:30:18 +0100

    existing code import. Closes #1

---
+++ WalkChat/WalkChat/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WalkChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbLog = new System.Windows.Forms.TextBox();
+            this.tbMsg = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbLog
+            // 
+            this.tbLog.Location = new System.Drawing.Point(12, 12);
+            this.tbLog.Multiline = true;
+            this.tbLog.Name = "tbLog";
+            this.tbLog.ReadOnly = true;
+            this.tbLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbLog.Size = new System.Drawing.Size(498, 287);
+            this.tbLog.TabIndex = 0;
+            this.tbLog.Text = "Welcome to Walkie Chatie!";
+            // 
+            // tbMsg
+            // 
+            this.tbMsg.Location = new System.Drawing.Point(13, 306);
+            this.tbMsg.Multiline = true;
+            this.tbMsg.Name = "tbMsg";
+            this.tbMsg.Size = new System.Drawing.Size(497, 59);
+            this.tbMsg.TabIndex = 1;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(227, 371);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 404);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.tbMsg);
+            this.Controls.Add(this.tbLog);
+            this.Name = "Form1";
+            this.Text = "Walkie Chatie";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbLog;
+        private System.Windows.Forms.TextBox tbMsg;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ WalkChat/WalkChat/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace WalkChat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.thread = new Thread(new ThreadStart(this.UdpRead));
+            this.thread.Start();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Send datagram
+                UdpClient udpSendClient = new UdpClient();
+                udpSendClient.Connect(IPAddress.Parse("6.6.6.255"), 6661);
+//                udpSendClient.Connect(IPAddress.Parse("127.0.0.1"), 6661);
+                Byte[] sendBytes = Encoding.ASCII.GetBytes(tbMsg.Text);
+                udpSendClient.Send(sendBytes, sendBytes.Length);
+                udpSendClient.Close();
+            }
+            catch (Exception ex)
+            {
+                AppendText("E:" + ex.ToString());
+            }
+
+            // Add to log
+            tbLog.Text += "\r\n" + "T:" + tbMsg.Text;
+            tbMsg.Text = "";
+        }
+
+        private Thread thread = null;
+        private UdpClient udpReadClient = null;
+        private void UdpRead()
+        {
+            try
+            {
+                udpReadClient = new UdpClient(6661);
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 6661);
+                while (true)
+                {
+                    Byte[] receiveBytes = udpReadClient.Receive(ref RemoteIpEndPoint);
+                    this.AppendText("R:" + Encoding.ASCII.GetString(receiveBytes));
+                }
+            }
+            catch (Exception ex)
+            {
+                AppendText("E:" + ex.ToString());
+            }
+        }
+        delegate void SetTextCallback(string text);
+        private void AppendText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.tbLog.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(AppendText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.tbLog.Text += "\r\n" + text;
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            this.udpReadClient.Close();
+        }
+    }
+}
